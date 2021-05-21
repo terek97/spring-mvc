aa3180dbc8c5841a9591ecbba27ac72ea5c5851c
terek97 <OiL1119002OiL>
2021-05-21 17:31:47 +0400

    table in HTML

--- 2.2.2. spring_mvc/src/main/java/web/models/Car.java
+++ 2.2.2. spring_mvc/src/main/java/web/models/Car.java
@@ -19,6 +19,14 @@ public class Car {
     public Car() {
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public Car(String brand, int year) {
         this.brand = brand;
         this.year = year;
